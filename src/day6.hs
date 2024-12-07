d7486fde5fde0cdec1eb3c673aa253bca17434a6
Axel Hammarberg <axeha009@student.liu.se>
2024-12-07 21:32:15 +0100

    Done day 6

---
+++ src/day6.hs
@@ -0,0 +1,63 @@
+import Data.List (find)
+import Data.Maybe (fromJust, isNothing)
+import Data.Set (Set, empty, insert, member, toList)
+
+
+main :: IO()
+main = do
+    input <- readFile "input/input6.txt"
+    let content = lines input
+    let grid = concat content
+
+    let w = length $ head content
+    let h = length content
+
+    let (gx, gy) = findGuard grid w h
+    let path = walk grid w h gx gy 0 (-1)
+
+    let count = length path
+    let without_guard = filter (/= (gx, gy)) (toList path)
+
+    let res = length $ filter (hasLoop grid w h gx gy) without_guard
+
+    print count
+    print res
+
+
+atPos :: String -> Int -> Int -> Int -> Int -> Maybe Char
+atPos _ w h x y | (x < 0) || (y < 0) || (x >= w) || (y >= h) = Nothing
+atPos s w _ x y = Just $ s !! (x + (w * y))
+
+
+findGuard :: String -> Int -> Int -> (Int, Int)
+findGuard s w h = pos where
+    coords = [(x, y) | x <- [0..w - 1], y <- [0..h - 1]]
+    pos = fromJust $ find (\(x, y) -> atPos s w h x y == Just '^') coords
+
+
+turn :: (Int, Int) -> (Int, Int)
+turn (dx, dy) = case (dx, dy) of 
+                   (0, -1) -> (1, 0)
+                   (1, 0) -> (0, 1)
+                   (0, 1) -> (-1, 0)
+                   (-1, 0) -> (0, -1)
+                   _ -> (0, 0)
+
+
+walk :: String -> Int -> Int -> Int -> Int -> Int -> Int -> Set (Int, Int)
+walk grid w h x y _ _ | isNothing (atPos grid w h x y) = empty
+walk grid w h x y dx dy | atPos grid w h (x + dx) (y + dy) == Just '#' = turned where
+    (ndx, ndy) = turn (dx, dy)
+    turned = walk grid w h x y ndx ndy
+walk grid w h x y dx dy = insert (x, y) next where
+    next = walk grid w h (x + dx) (y + dy) dx dy
+
+
+hasLoop :: String -> Int -> Int -> Int -> Int -> (Int, Int) -> Bool
+hasLoop grid w h xp yp (bx, by) = loop xp yp (0, -1) empty where
+    blocked x1 y1 = ((x1, y1) == (bx, by)) || (atPos grid w h x1 y1 == Just '#')
+    loop x y (dx, dy) visited
+      | isNothing (atPos grid w h x y) = False
+      | member (x, y, dx, dy) visited = True
+      | blocked (x + dx) (y + dy) = loop x y (turn (dx, dy)) visited
+      | otherwise = loop (x + dx) (y + dy) (dx, dy) (insert (x, y, dx, dy) visited)
