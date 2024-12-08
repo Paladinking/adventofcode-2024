320a3620d3bf216183f98742cbdc24b603a8464b
Axel Hammarberg <axeha009@student.liu.se>
2024-12-08 11:30:00 +0100

    Much faster day 6

--- src/day6.hs
+++ src/day6.hs
@@ -1,13 +1,16 @@
 import Data.List (find)
 import Data.Maybe (fromJust, isNothing)
 import Data.Set (Set, empty, insert, member, toList)
+import Data.ByteString (ByteString, index)
+import Data.ByteString.Char8 (pack)
+import Data.Word (Word8)
 
 
 main :: IO()
 main = do
     input <- readFile "input/input6.txt"
     let content = lines input
-    let grid = concat content
+    let grid = pack $ concat content
 
     let w = length $ head content
     let h = length content
@@ -24,15 +27,15 @@ main = do
     print res
 
 
-atPos :: String -> Int -> Int -> Int -> Int -> Maybe Char
+atPos :: ByteString -> Int -> Int -> Int -> Int -> Maybe Word8
 atPos _ w h x y | (x < 0) || (y < 0) || (x >= w) || (y >= h) = Nothing
-atPos s w _ x y = Just $ s !! (x + (w * y))
+atPos s w _ x y = Just $ index s (x + (w * y))
 
 
-findGuard :: String -> Int -> Int -> (Int, Int)
+findGuard :: ByteString -> Int -> Int -> (Int, Int)
 findGuard s w h = pos where
     coords = [(x, y) | x <- [0..w - 1], y <- [0..h - 1]]
-    pos = fromJust $ find (\(x, y) -> atPos s w h x y == Just '^') coords
+    pos = fromJust $ find (\(x, y) -> atPos s w h x y == Just 94) coords
 
 
 turn :: (Int, Int) -> (Int, Int)
@@ -44,18 +47,18 @@ turn (dx, dy) = case (dx, dy) of
                    _ -> (0, 0)
 
 
-walk :: String -> Int -> Int -> Int -> Int -> Int -> Int -> Set (Int, Int)
+walk :: ByteString -> Int -> Int -> Int -> Int -> Int -> Int -> Set (Int, Int)
 walk grid w h x y _ _ | isNothing (atPos grid w h x y) = empty
-walk grid w h x y dx dy | atPos grid w h (x + dx) (y + dy) == Just '#' = turned where
+walk grid w h x y dx dy | atPos grid w h (x + dx) (y + dy) == Just 35 = turned where
     (ndx, ndy) = turn (dx, dy)
     turned = walk grid w h x y ndx ndy
 walk grid w h x y dx dy = insert (x, y) next where
     next = walk grid w h (x + dx) (y + dy) dx dy
 
 
-hasLoop :: String -> Int -> Int -> Int -> Int -> (Int, Int) -> Bool
+hasLoop :: ByteString -> Int -> Int -> Int -> Int -> (Int, Int) -> Bool
 hasLoop grid w h xp yp (bx, by) = loop xp yp (0, -1) empty where
-    blocked x1 y1 = ((x1, y1) == (bx, by)) || (atPos grid w h x1 y1 == Just '#')
+    blocked x1 y1 = ((x1, y1) == (bx, by)) || (atPos grid w h x1 y1 == Just 35)
     loop x y (dx, dy) visited
       | isNothing (atPos grid w h x y) = False
       | member (x, y, dx, dy) visited = True
